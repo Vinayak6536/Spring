e804a5e15579ce557b4fd6b8543392a05c6b163d
vinayak6536 <vinayakgh45@gmail.com>
2026-01-07 16:00:06 +0530

    CRUD Oper using Persistence....

--- event-system-2/src/main/java/com/xworkz/event/Runner.java
+++ event-system-2/src/main/java/com/xworkz/event/Runner.java
@@ -6,7 +6,9 @@ import com.xworkz.event.service.EventService;
 import com.xworkz.event.service.impl.EventServiceImpl;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Runner {
     public static void main(String[] args) {
@@ -16,13 +18,31 @@ public class Runner {
 
        // eventService.validateAndSave(dto1);
 
-        Optional<EventDto> eventDto=eventService.getEventInfo(1);
-        System.out.println(eventDto.get());
+      //  Optional<EventDto> eventDto=eventService.getEventInfo(1);
+       // System.out.println(eventDto.get());
 
         boolean update=eventService.updateTimeById(1,11);
         System.out.println(update);
 
         boolean delete=eventService.deleteById(1);
         System.out.println(delete);
+
+        EventDto eventDto1=eventService.getEventByName("magic-show");
+        System.out.println(eventDto1);
+
+        List<EventEntity> eventDto=eventService.getEventByTime(10);
+        Stream.of(eventDto).forEach(System.out::println);
+
+        Object[] getName=eventService.getEventManagerAndLocationByName("magic-show");
+        Stream.of(getName).forEach(System.out::println);
+
+        Stream.of(eventService.getAllEventlist()).forEach(System.out::println);
+
+        Object dev=eventService.getLocationByTime(10);
+        System.out.println(dev);
+
+        System.out.println(eventService.getEventNameByManager("Chaitanya"));
+
+        Stream.of(eventService.getAllManagerName()).forEach(System.out::println);
     }
 }
