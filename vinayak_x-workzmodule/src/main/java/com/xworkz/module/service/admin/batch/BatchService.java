9515f67561db4ae9401d3ef4d5dcf2a65c94600a
vinayak6536 <vinayakgh45@gmail.com>
2026-02-18 22:35:43 +0530

    using Encryption and Decryption.....

--- vinayak_x-workzmodule/src/main/java/com/xworkz/module/service/admin/batch/BatchService.java
+++ vinayak_x-workzmodule/src/main/java/com/xworkz/module/service/admin/batch/BatchService.java
@@ -3,11 +3,12 @@ package com.xworkz.module.service.admin.batch;
 import com.xworkz.module.dto.BatchDto;
 import com.xworkz.module.dto.StudentDto;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 public interface BatchService {
-    boolean validateAndSave(BatchDto batchDto);
+    boolean validateAndSave(BatchDto batchDto) throws IOException;
 
     List<BatchDto> getBatchList();
 
--- vinayak_x-workzmodule/src/main/java/com/xworkz/module/service/admin/batch/impl/BatchServiceImpl.java
+++ vinayak_x-workzmodule/src/main/java/com/xworkz/module/service/admin/batch/impl/BatchServiceImpl.java
@@ -2,12 +2,18 @@ package com.xworkz.module.service.admin.batch.impl;
 
 import com.xworkz.module.dto.BatchDto;
 import com.xworkz.module.entity.BatchEntity;
+import com.xworkz.module.entity.ImageEntity;
 import com.xworkz.module.repository.admin.batch.BatchRepository;
 import com.xworkz.module.service.admin.batch.BatchService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,10 +25,34 @@ public class BatchServiceImpl implements BatchService {
 
     @Autowired
     private BatchEntity batchEntity;
+
+    @Autowired
+    private ImageEntity imageEntity;
+
     @Override
-    public boolean validateAndSave(BatchDto batchDto) {
+    public boolean validateAndSave(BatchDto batchDto) throws IOException {
         if (batchDto != null) {
+
+            MultipartFile multipartFile=batchDto.getFile();
             BeanUtils.copyProperties(batchDto,batchEntity);
+            String uploadDir = "V:/Spring/GitBash/Spring/vinayak_x-workzmodule/UploadedImages/";
+
+            String fileName = System.currentTimeMillis() + "_" + multipartFile.getOriginalFilename();
+
+            Path path = Paths.get(uploadDir + fileName);
+            Files.write(path, multipartFile.getBytes());
+
+
+
+            imageEntity.setOriginalName(multipartFile.getOriginalFilename());
+            imageEntity.setFileSize(multipartFile.getSize());
+            imageEntity.setImagePath(fileName);
+            imageEntity.setContentType(multipartFile.getContentType());
+
+            imageEntity.setBatch(batchEntity);
+
+            batchEntity.setImageLogo(imageEntity);
+
            return batchRepository.save(batchEntity);
         }
         return false;
@@ -32,12 +62,19 @@ public class BatchServiceImpl implements BatchService {
     public List<BatchDto> getBatchList() {
 
         List<BatchEntity> entityList = batchRepository.getBatchList();
+        System.out.println(entityList);
         List<BatchDto> dtoList = new ArrayList<>();
 
         for (BatchEntity entity : entityList) {
             System.out.println(entity.getStudents());
             BatchDto dto = new BatchDto();
             BeanUtils.copyProperties(entity, dto);
+
+            // ✅ IMPORTANT: set image path manually
+            if (entity.getImageLogo() != null) {
+                dto.setImagePath(entity.getImageLogo().getImagePath());
+            }
+
             dtoList.add(dto);
         }
 
